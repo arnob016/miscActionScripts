50ad44784fc2c6ab3ad91eaf49ab7182788238b2
Biplob Sutradhar <biplobsd11@gmail.com>
2021-12-29 15:49:32 +0600

    trying to fetch 5 times

--- diu/result/getResultList.py
+++ diu/result/getResultList.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     url = "http://software.diu.edu.bd:8189/result"
     getIdList = rawDataFIds()
     realData = readResult()
-    for _ in range(2):
+    for _ in range(5):
         print(len(getIdList), end="-")
         
         for i,p in enumerate(getIdList):
